72f0aee80c1bae8325437b6e355fd12a6709d528
JuliaCarew <crewjulia3@gmail.com>
2024-10-29 12:31:42 -0300

    set up game area + door, winbox triggers, started light trigger & added cube for push

--- Assets/Scripts/TriggerControl.cs
+++ Assets/Scripts/TriggerControl.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class TriggerControl : MonoBehaviour
 {
+    public GameObject DoorOBS;
+    public GameObject Wall_L_02;
+    public GameObject Winbox;
+    public GameObject HallLight;
+
+    public void Start()
+    {
+        DoorOBS.SetActive(true);
+        Wall_L_02.SetActive(true);
+        // set Winbox emission OFF
+        Winbox.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+        Winbox.GetComponent<MeshRenderer>().material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
+        
+        HallLight.SetActive(false);
+    }
     public void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger");
+        DoorOBS.SetActive(false);
+        Wall_L_02.SetActive(false);
+        // set Winbox emission ON
+        Winbox.GetComponent<MeshRenderer>().material.EnableKeyword("_EMISSION");
+        Winbox.GetComponent<MeshRenderer>().material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.None;
+
+        HallLight.SetActive(true);
+
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        // set Winbox emission OFF
+        Winbox.GetComponent<MeshRenderer>().material.DisableKeyword("_EMISSION");
+        Winbox.GetComponent<MeshRenderer>().material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
     }
 }
